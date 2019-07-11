157a768493e636e87149d09e9940e80283b34a30
ttjb2 <tom.bauer95@gmail.com>
2019-07-11 16:20:46 -0500

    readme.md

--- src/App.js
+++ src/App.js
@@ -6,22 +6,53 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      score: []
+      score: [],
+      playerName: ""
     };
   }
 
   getScores = () => {
     axios("/score").then(response => {
+      console.log("response: ", response);
+
       this.setState({
         score: response.data
       });
     });
   };
 
+  createPlayer = event => {
+    if (event.keyCode === 13) {
+      axios
+        .post("/player", {
+          playerNme: this.state.playerName
+        })
+        .then(function(response) {
+          console.log(response);
+        });
+    } else {
+      this.setState({
+        playerName: event.target.value
+      });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <div onClick={e => this.getScores()}>get all scores</div>
+        <input
+          type="text"
+          onKeyUp={e => this.createPlayer(e)}
+          placeholder="Enter New Player Name Here"
+        />
+        {this.state.score.map(score => {
+          return (
+            <div>
+              {score.playerNme} {score.score}
+            </div>
+          );
+        })}
       </div>
     );
   }
